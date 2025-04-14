696d213ed617ba4e546eef386de680911f5d1c27
StrangerSeemanta <seemanta01@gmail.com>
2025-04-14 19:55:21 +0600

    Ambika Releaseed

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://prdncygaxhkdoagbucxy.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByZG5jeWdheGhrZG9hZ2J1Y3h5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5MDEyMTUsImV4cCI6MjA1ODQ3NzIxNX0.pybIOhzaVjPKpTuCZGQjSAGdld1v6WUf8-NPn3DdisA";
 export const supabase = createClient(supabaseUrl, supabaseKey);
